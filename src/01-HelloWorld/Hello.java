2a99c0ea0d0600766f8b95fb571655403e94a399
Ayub Abdi <ayubabdi6002@gmail.com>
2026-08-02 05:19:37 +0800

    add how to print different output

---
+++ src/01-HelloWorld/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("printed second line");
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+    }
+}
