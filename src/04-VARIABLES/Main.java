5e9f0236564fa6782b6ea1d14b2f24b8b4822a1d
Ayub Abdi <ayubabdi6002@gmail.com>
2026-08-03 02:12:35 +0800

    all about varibales

--- src/04-VARIABLES/Main.java
+++ src/04-VARIABLES/Main.java
@@ -12,6 +12,29 @@ public class Main {
         System.out.println(myLetter);
         System.out.println(myBool);
         System.out.println(myText);
+
+
+        // we can also assign multiple variables the same value in one line
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        // this is a practical example of how to use variables in a program to store studebnt information
+        // Student data
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
     }
     
 }
